9ce653deeb4a2e6a6ea5254f78c9c221896726e0
mikrohash.de <iota@mikrohash.de>
2018-03-05 21:49:07 +0100

    delete stupid file; updated go code

--- pow.go
+++ pow.go
@@ -8,40 +8,21 @@ import "time"
 import "strconv"
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	const maxTimestampTrytes = "MMMMMMMMM"
+	const TimestampTrinarySize = 27
 
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	giota.PowProcs, _ = strconv.Atoi(scanner.Text());
 
-	scanner.Scan()
-	powId, _ := strconv.Atoi(scanner.Text());
-
 	powName, pow := giota.GetBestPoW()
-
-	if powId == 0 {
-		fmt.Println(powName)
-	}
-
-	if powId == 1 {
-		pow = giota.PowGo
-	}
-
-	if powId == 2 {
-		pow = giota.PowC
-	}
-
-	if powId == 3 {
-		pow = giota.PowSSE
-	}
+	fmt.Println(powName)
 
 	for true {
 		scanner.Scan()
 		input := scanner.Text()
 		trytes, _ := giota.ToTrytes(input)
 		tx, _ := giota.NewTransaction(trytes)
-
-		const maxTimestampTrytes = "MMMMMMMMM"
-		const TimestampTrinarySize = 27
 
 		timestamp := giota.Int2Trits(time.Now().UnixNano()/1000000, TimestampTrinarySize).Trytes()
 		tx.AttachmentTimestamp = timestamp
